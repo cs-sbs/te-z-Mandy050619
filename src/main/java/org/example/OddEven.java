f9153e9e2874610660f15d67c80fdd9074e6133a
Mandy050619 <547165916@qq.com>
2024-12-29 15:59:23 +0800

    Update OddEven.java

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -1,28 +1,24 @@
-package org.example;
-
 import java.util.Scanner;
 
 public class OddEven {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int oddCount = 0; // 奇数计数器
-        int evenCount = 0; // 偶数计数器
+        int oddCount = 0;
+        int evenCount = 0;
 
         while (true) {
             int number = scanner.nextInt();
             if (number == -1) {
-                break; // 输入-1表示结束
+                break;
             }
             if (number % 2 == 0) {
-                evenCount++; // 偶数计数器加1
+                evenCount++;
             } else {
-                oddCount++; // 奇数计数器加1
+                oddCount++;
             }
         }
-        scanner.close();
 
-        // 输出结果
         System.out.println(oddCount + " " + evenCount);
+        scanner.close();
     }
 }
